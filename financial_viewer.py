4680ded3ccabfd67411e68a9da6e3d362178f11b
hmcgough <hrmagoo@gmail.com>
2021-07-13 23:11:56 -0700

    group data by day and plot accumulated daily spending

--- financial_viewer.py
+++ financial_viewer.py
@@ -1,16 +1,34 @@
 from sys import argv
-from os import listdir
-from os import walk
-from os import path
+import os
+import matplotlib.pyplot as plt
+import pandas as pd
+import matplotlib.dates as mdates
+
 
 def get_files_in_folder(path):
     result = []
 
-    for a in walk(path, False):
-        print(a)
+    for a in os.walk(path, False):
+        if len(a[2]) == 0:
+            break
+        for file in a[2]:
+            result.append(os.path.join(a[0], file))
 
-    for f in listdir(folder):
-        print(f)
+    return result
+
+
+def read_money(money):
+    try:
+        return float(money.replace('$', ''))
+    except:
+        return money
+
+def daily(tss):
+    result = [0] * len(tss)
+    for i in range(len(tss)):
+        result[i] = tss[i].to_period('D').to_timestamp()
+
+    return pd.DatetimeIndex(result)
 
 
 if __name__ == '__main__':
@@ -18,5 +36,31 @@ if __name__ == '__main__':
     if argv is not None and len(argv) > 1:
         folder = argv[1]
 
-    get_files_in_folder(folder)
+    files = get_files_in_folder(folder)
+
+    df = pd.DataFrame()
+
+    for f in files:
+        df_read = pd.read_csv(f, header=0, converters={'Amount': read_money}, parse_dates=['Transaction Date'], index_col=0)
+        df = pd.concat([df, df_read])
+
+    spending = df[(df['Amount'] > 0)]
+
+    # spending = spending.groupby(['Transaction Date'])['Amount'].sum()
+    spending = spending.groupby(daily)['Amount'].sum()
+
+    cumsum = spending.cumsum()
+
+    fig, ax = plt.subplots(1)
+
+    #zerothElementInIndex = [i[0] for i in cumsum.index]
+    ax.plot(cumsum.index, cumsum, 'x-k')
+
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%b %d'))
+    # ax.xaxis.set_major_locator(mdates.DayLocator(interval=1))
+    ax.xaxis_date()
+    fig.autofmt_xdate()
+
+    plt.show()
+
 
